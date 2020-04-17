e95a226fd63c5b582a890549da82ba9a335091ec
Bob van de Vijver <bob@drenso.nl>
2020-04-17 17:01:01 +0200

    Add truncate filter

--- src/plugins/text/plugin.ts
+++ src/plugins/text/plugin.ts
@@ -26,4 +26,32 @@ export default function install(_vue: VueConstructor) {
     value = value.toString();
     return value.charAt(0).toUpperCase() + value.slice(1);
   });
+
+  _vue.filter('truncate', (value: any, maxLength: number = 50) => {
+    if (typeof value !== 'string') {
+      return;
+    }
+
+    // Remove dot characters from max length
+    maxLength = maxLength - 3;
+
+    if (value.length < maxLength) {
+      return value;
+    }
+
+    let truncated = '';
+    const valueArray = value.split(' ');
+    let valueWordCount = 0;
+    for (const word of valueArray) {
+      valueWordCount += word.length;
+      if (valueWordCount > maxLength) {
+        return truncated + '...';
+      }
+
+      truncated += ' ' + word;
+      valueWordCount++;
+    }
+
+    return truncated + '...';
+  });
 }
