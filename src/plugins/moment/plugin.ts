6ed45592c0c37008e92a11372d561667ecabf97c
Tobias Feijten <tobias@drenso.nl>
2020-05-06 19:50:43 +0200

    Fix error when no options are given to moment plugin

--- src/plugins/moment/plugin.ts
+++ src/plugins/moment/plugin.ts
@@ -8,7 +8,8 @@ export interface MomentOptions {
   default_tz?: string;
 }
 
-export default function install(_vue: VueConstructor, options: MomentOptions) {
+export default function install(_vue: VueConstructor, options?: MomentOptions) {
+  options = options || {};
   if (options.default_tz) {
     moment.tz.setDefault(options.default_tz);
     // @ts-ignore
